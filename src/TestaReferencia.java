fdfb700a6f0d8328251da12dd40b40d68d75f4c3
Inácio Fabrício Nunes de Sousa <fabricionunes.si@hotmail.com>
2020-10-30 17:41:27 -0300

    Commit Inicial

---
+++ src/TestaReferencia.java
@@ -0,0 +1,28 @@
+
+public class TestaReferencia {
+
+	public static void main(String[] args) {
+
+		Gerente g1 = new Gerente();
+		g1.setNome("José");
+		g1.setSalario(5000);
+		
+		Funcionario f1 = new Funcionario();
+		f1.setNome("Maria");
+		f1.setSalario(2000);
+		
+		EditorVideo e1 = new EditorVideo();
+		f1.setNome("Josefa");
+		f1.setSalario(2500);
+		
+		ControleBonificacao controle = new ControleBonificacao();
+		controle.registra(g1);
+		controle.registra(f1);
+		controle.registra(e1);
+		
+		System.out.println(controle.getSoma());
+		
+		
+	}
+
+}
